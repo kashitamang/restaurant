cafcf585207ee1d4ea59c5cfe76b0c0c9f35b3db
KASHI TAMANG <tamang.kashi@gmail.com>
2022-06-24 12:47:55 -0700

    adding header in app.js, also adds components: create, home, item, list, and update. imports components into app.js as well as base libaries. renders to page, and links are working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+//styles
 import './App.css';
+//libraries
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+// *** import { useState } from 'react';
 
-function App() {
+//services
+// **** import { logout } from './services/fetch-utils';
+//***then add to onclick on logout button */
+
+//components
+import HomePage from './components/HomePage';
+import ListPage from './components/ListPage';
+import CreatePage from './components/CreatePage';
+import UpdatePage from './components/UpdatePage';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/list">List Page</Link>
+            </li>
+            <li>
+              <Link to="/create">Create Page</Link>
+            </li>
+            <button to="/users">Logout</button>
+          </ul>
+        </nav>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path='/'>
+            <HomePage />
+          </Route>
+          <Route exact path='/list'>
+            <ListPage />
+          </Route>
+          <Route exact path='/create'>
+            <CreatePage />
+          </Route>
+          <Route exact path='/update/:id'>
+            <UpdatePage />
+          </Route>
+        </Switch>
       </header>
-    </div>
+    </Router>
   );
 }
-
-export default App;
